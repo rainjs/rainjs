763bad4a451759884791aa8929601ab2b7edede1
Răzvan Mocanu <razvan.mocanu@gmail.com>
2011-11-30 19:56:07 +0200

    Subsequent view requests domId rewrite and controller script execution

--- components/core-components/htdocs/js/raintime/subsequent_views.js
+++ components/core-components/htdocs/js/raintime/subsequent_views.js
@@ -32,13 +32,62 @@ define(["require", "core-components/client_util"],
             domId = this.viewContext.moduleId,
             componentParts,
             componentRoot,
+            componentParent,
+            controllerScripts,
             dependencies,
-            head = $("head");
+            head = $("head"),
+            domIdOffset = 0,
+            reDomIds;
+
+        Registry.deregister(domId);
 
         componentParts = component.content.match(/<body>\s*(<div[^>]*>[\s\S]*<\/div>)\s*<\/body>/);
         if (componentParts && componentParts.length === 2) {
-            componentRoot = $(componentParts[1]).attr("data-instanceid", domId);
-            this.root.replaceWith(componentRoot);
+            componentRoot = $(componentParts[1]);
+            this.root.replaceWith(componentRoot.not("script"));
+
+            for (var registeredComponent in Registry.components) {
+                domIdOffset++;
+            }
+
+            // Update the data-instanceid attributes of subcomponents
+            componentRoot.first().find("[data-instanceid]").andSelf().each(function () {
+                $(this).attr("data-instanceid",
+                             domIdOffset + parseInt($(this).attr("data-instanceid"), 10));
+            });
+
+            // Update dom ids inside controller scripts and evaluate them
+            componentRoot.filter("script").each(function () {
+                var scriptText = $(this).text();
+
+                scriptText = scriptText.replace(/\((\d+)\)/g,
+                    function(matchString, domId) {
+                        return "(" + (parseInt(domId, 10) + domIdOffset) + ")";
+                    }
+                );
+
+                scriptText = scriptText.replace(/pre-component-(\d+)/,
+                    function (matchString, domId) {
+                        return "pre-component-" + (parseInt(domId, 10) + domIdOffset);
+                    }
+                );
+                
+                scriptText = scriptText.replace(/post-component-(\d+)/,
+                    function (matchString, domId) {
+                        return "post-component-" + (parseInt(domId, 10) + domIdOffset);
+                    }
+                );
+
+                scriptText = scriptText.replace(/"domId":(\d+)/,
+                    function (matchString, domId) {
+                        return '"domId":' + (parseInt(domId, 10) + domIdOffset);
+                    }
+                );
+
+                $(this).text(scriptText);
+
+                new Function(scriptText)();
+            });
         }
 
         dependencies = component.dependencies.css;
@@ -46,12 +95,6 @@ define(["require", "core-components/client_util"],
             head.append('<link rel="stylesheet" type="text/css" href="/resources?files='
                     + encodeURIComponent(dependencies.join(";")) + '">');
         }
-
-        Registry.deregister(domId);
-        Registry.register({
-            domId: domId,
-            clientcontroller: component.clientcontroller
-        });
     }
 
     return SubsequentViewHandler;
