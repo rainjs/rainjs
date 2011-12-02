932731487a4a572faccb53bcd47a9d4a983532da
Răzvan Mocanu <razvan.mocanu@gmail.com>
2011-12-02 21:42:22 +0200

    Fixed wizard example

--- components/wizard/htdocs/controller/final.js
+++ components/wizard/htdocs/controller/final.js
@@ -1,9 +1,12 @@
 define(function () {
     function init() {
+        var root = this.viewContext.getRoot();
+        
         var name = this.viewContext.storage.get('name', true);
         var age = this.viewContext.storage.get('age', true);
 
-        $('body').html('<p>Welcome ' + name + '!</p><p>You are ' + age + ' years old</p>');
+        root.find('.name').text(name);
+        root.find('.age').text(age);
 
         this.viewContext.storage.remove('name', true);
         this.viewContext.storage.remove('age', true);
