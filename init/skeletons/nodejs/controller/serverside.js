22a46ae97b7fe287fdd1d64723f36b3697dbc89d
Alexandru Bularca <alexandru.bularca@1and1.ro>
2012-01-24 11:42:36 +0200

    documented the server side controller skel

--- init/skeletons/nodejs/controller/serverside.js
+++ init/skeletons/nodejs/controller/serverside.js
@@ -1,17 +1,41 @@
+/**
+ * Resolves the GET HTTP verb
+ *
+ * @param {Request} request the request object
+ * @param {Response} response the respons object
+ */
 function doGet (request, response) {
-  
+
 }
 
+/**
+ * Resolves the POST HTTP verb
+ *
+ * @param {Request} request the request object
+ * @param {Response} response the respons object
+ */
 function doPost (request, response) {
-  
+
 }
 
+/**
+ * Resolves the DELETE HTTP verb
+ *
+ * @param {Request} request the request object
+ * @param {Response} response the respons object
+ */
 function doDelete (request, response) {
-  
+
 }
 
+/**
+ * Resolves the PUT HTTP verb
+ *
+ * @param {Request} request the request object
+ * @param {Response} response the respons object
+ */
 function doPut (request, response) {
-  
+
 }
 
 exports.get = doGet;
