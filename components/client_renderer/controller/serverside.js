1068fbf240a7998cf69dbac7d5d774817694e4eb
Mitko Tschimev <privat@tschimev.de>
2012-02-24 15:16:24 +0200

    added mockdata

--- components/client_renderer/controller/serverside.js
+++ components/client_renderer/controller/serverside.js
@@ -4,7 +4,18 @@
  * @param {Request} request the request object
  * @param {Response} response the response object
  */
-function doGet(request, response) {}
+function doGet(request, response) {
+    var data = {
+        css: [ '/component/textbox/htdocs/css/index.css' ],
+        controller: '/components/textbox/htdocs/controller/index.js',
+        domId: 500,
+        instanceId : 'b367fd0ee2d8432c202e8af61c288220c420fef3',
+        html : ''
+    };
+
+    response.writeHead(statusCode, {"Content-Type": "application/jsonp"});
+    response.end('ClientRenderer.renderComponent('+JSON.stringify(data)+')');
+}
 
 /**
  * Resolves the POST HTTP verb.
