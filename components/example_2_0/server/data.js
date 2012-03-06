de2d1406c56d0cfa9826c499be4dd0079885bcca
Ciprian Roman <roudolph@gmail.com>
2012-03-06 13:55:34 +0200

    Rewrite urls in css files at precompile time.

---
+++ components/example_2_0/server/data.js
@@ -0,0 +1,24 @@
+/**
+ * Template data
+ */
+function index(callback, data) {
+    callback(null, data);
+}
+
+function nasty_level2(callback, data) {
+    setTimeout(function(){
+        callback(null, data);
+    }, Math.floor(Math.random()*3000));
+}
+
+function nasty_level3(callback, data) {
+    setTimeout(function(){
+        callback(null, data);
+    }, Math.floor(Math.random()*3000));
+}
+
+module.exports = {
+    index: index,
+    nasty_level2: nasty_level2,
+    nasty_level3: nasty_level3
+};
---
+++ components/example_2_0/server/websockets/example_socket.js
@@ -0,0 +1,8 @@
+function handle(socket) {
+    socket.emit('hello', {message: 'Hello Sockets!'});
+};
+
+module.exports = {
+    channel: 'example',
+    handle: handle
+};
