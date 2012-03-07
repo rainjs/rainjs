55c44f18d8cb8320eca64f4228687ca44f873f88
Ciprian Roman <roudolph@gmail.com>
2012-03-07 10:08:07 +0200

    Removed old files that are no longer relevant.

--- playground/slowserver.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-	setTimeout(function (){
-  res.end('Hello World\n');
-}, 2000);
-}).listen(1338, "127.0.0.1");
-console.log('slow server started');
