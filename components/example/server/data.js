3bffd708985622738eb6f4209baf74bce49aff58
Mitko Tschimev <privat@tschimev.de>
2012-03-08 17:05:31 +0200

    Catching code errors from data.js

--- components/example/server/data.js
+++ components/example/server/data.js
@@ -13,14 +13,19 @@ function nasty_level2(callback, data) {
 
 function nasty_level3(callback, data) {
     setTimeout(function(){
-        callback({
-            arrrghhhhhh: "DAMMNNNNNNNNNN"
-        }, data);
+        callback(null, data);
     }, Math.floor(Math.random()*3000));
+}
+
+function with_customer_error(callback, data){
+    callback({
+        arrrghhhhhh: "DAMMNNNNNNNNNN"
+    }, data);
 }
 
 module.exports = {
     index: index,
     nasty_level2: nasty_level2,
-    nasty_level3: nasty_level3
+    nasty_level3: nasty_level3,
+    with_customer_error: with_customer_error
 };
