0a00da9895601fe31d91a6407a523bbc15cd04fe
Ciprian Roman <roudolph@gmail.com>
2012-03-08 19:39:02 +0200

    Added JSDoc for the Raintime and finished the lifecycle.

--- components/core/client/js/raintime/client_rendering.js
+++ components/core/client/js/raintime/client_rendering.js
@@ -5,8 +5,8 @@ define([
 ], function(Promise, messaging, raintime) {
     /**
      * The ClientRenderer handles the registration and inserting of new components from the server.
-     * A placeholder is replaced if a component is not in time
-     * This works for all transport layers
+     * A placeholder is replaced if a component is not in time.
+     * This works for all transport layers.
      *
      * @name ClientRenderer
      * @class A ClientRenderer instance
@@ -20,38 +20,36 @@ define([
     }
 
     /**
-     * Sets the placeholder component
+     * Sets the placeholder component.
      *
-     * @param {Object} component The whole rendered placeholder component
+     * @param {Object} component the whole rendered placeholder component
      */
     ClientRenderer.prototype.setPlaceholder = function (component) {
         this.placeholderComponent = component;
     };
 
     /**
-     * Sets the placeholder timeout which is set from the server configuration
+     * Sets the placeholder timeout which is set from the server configuration.
      *
-     * @param {Number} milliseconds Time in milliseconds
+     * @param {Number} milliseconds time in milliseconds
      */
     ClientRenderer.prototype.setPlaceholderTimeout = function (milliseconds) {
         this.placeholderTimeout = milliseconds;
     };
 
     /**
-     * Renders a component
+     * Renders a component.
      *
-     * @param {Object} component The rendered component
-     * @param instanceId The instance id of the component
-     *                   This is sent only if a placeholder is rendered
+     * @param {Object} component the rendered component
      */
     ClientRenderer.prototype.renderComponent = function (component) {
         insertComponent(this, component);
     };
 
     /**
-     * Renders a placeholder
+     * Renders a placeholder.
      *
-     * @param {String} instanceId The instanceId of the component for the placeholder
+     * @param {String} instanceId the instanceId of the component for the placeholder
      */
     ClientRenderer.prototype.renderPlaceholder = function (instanceId) {
         this.placeholderComponent.instanceId = instanceId;
@@ -59,12 +57,12 @@ define([
     };
 
     /**
-     * Insert the component to the dom and register it
+     * Insert the component to the DOM and registers it.
      *
-     * @param {ClientRenderer} self The instance of ClientRenderer
-     * @param {Object} component The rendered component
-     * @memberOf ClientRenderer#
+     * @param {ClientRenderer} self the class instance
+     * @param {Object} component the rendered component
      * @private
+     * @memberOf ClientRenderer#
      */
     function insertComponent(self, component) {
         var domElement = $('#' + component.instanceId);
@@ -74,8 +72,8 @@ define([
             'app-container ' + component.id + '_' + component.version.replace(/[\.]/g, '_')
         );
 
-        //register component
-        raintime.ComponentRegistry.register(component);
+        // Registers the component.
+        raintime.componentRegistry.register(component);
 
         loadCSS(this, component.css, function() {
             domElement.show();
@@ -83,41 +81,41 @@ define([
 
         for (var len = component.children.length, i = 0; i < len; i++) {
             var childComponent = component.children[i];
-            raintime.ComponentRegistry.preRegister(childComponent);
+            raintime.componentRegistry.preRegister(childComponent);
             placeholderTimeout(self, childComponent);
         }
     }
 
     /**
-     * Renders the placeholder if the component is not returned in time ( placeholderTimeout )
+     * Renders the placeholder if the component is not returned in time ( placeholderTimeout ).
      *
-     * @param {ClientRenderer} self The instance of ClientRenderer
-     * @param {Object} placeholder The placeholder component
-     * @memberOf ClientRenderer#
+     * @param {ClientRenderer} self the class instance
+     * @param {Object} placeholder the placeholder component
      * @private
+     * @memberOf ClientRenderer#
      */
-    function placeholderTimeout(self, placeholder){
+    function placeholderTimeout(self, placeholder) {
         setTimeout(function() {
             if (!$('#' + placeholder.instanceId).hasClass('app-container')) {
                 self.renderPlaceholder(placeholder.instanceId);
             }
         }, self.placeholderTimeout);
-    };
+    }
 
     /**
      * Load css files and insert html after the css files are completely loaded.
      * Maybe there is a better way. This works on IE8+, Chrome, FF, Safari.
      *
-     * @param {ClientRenderer} self The instance of ClientRenderer
+     * @param {ClientRenderer} self the class instance
      * @param {Array} css CSS dependencies
-     * @param {Function} callback Is invoked after all css dependencies are loaded
-     * @memberOf ClientRenderer#
+     * @param {Function} callback is invoked after all css dependencies are loaded
      * @private
+     * @memberOf ClientRenderer#
      */
     function loadCSS(self, css, callback) {
         var head = $('head');
         var loadedFiles = 0;
-        for ( var i = 0, len = css.length; i < len; i++) {
+        for (var i = 0, len = css.length; i < len; i++) {
             if (head.find("link[href='" + css[i] + "']").length > 0) {
                 if (++loadedFiles == css.length) {
                     callback();
@@ -141,7 +139,7 @@ define([
     }
 
     /**
-     * Export as a global
+     * Export as a global.
      */
     window.clientRenderer = new ClientRenderer();
 
--- components/core/client/js/raintime/raintime.js
+++ components/core/client/js/raintime/raintime.js
@@ -1,128 +1,280 @@
-define(['core/js/client_util',
-        'core/js/event_emitter',
+define(['core/js/event_emitter',
         'core/js/raintime/view_context'
-], function (ClientUtil, EventEmitter, ViewContext) {
+], function (EventEmitter, ViewContext) {
 
-    var raintime = (function () {
+    /**
+     * The Raintime instance.
+     *
+     * @type {Object}
+     * @property {ComponentRegistry} componentRegistry the component registry instance
+     * @private
+     */
+    var raintime = new Raintime();
+    raintime.componentRegistry = new ComponentRegistry();
 
-        var ComponentRegistry = (function () {
-            var _instance;
+    /**
+     * The map of registered components, indexed by the instanceId property.
+     *
+     * @type {Object}
+     * @private
+     */
+    var components = {};
 
-            function getInstance() {
+    /**
+     * The map of components for which the html content was not retrieved. When the html content
+     * arrives for a component, the component is moved to the ``components`` map.
+     * The components are indexed by the instanceId property.
+     *
+     * @type {Object}
+     * @private
+     */
+    var preComponents = {};
 
-                var components = {};
-                var preComponents = {};
+    /**
+     * The main client-side starting point used to access other libraries used in the controllers.
+     *
+     * @name Raintime
+     * @class This class is used by the ClientRenderer to register components.
+     * @constructor
+     */
+    function Raintime() {}
 
-                function Component(comp) {
-                    this.id = comp.id;
-                    this.version = comp.version;
-                    this.instanceId = comp.instanceId;
+    /**
+     * Creates a new component and sets default values.
+     *
+     * @name Component
+     * @class Keeps information about a component.
+     * @constructor
+     *
+     * @property {String} id the component id
+     * @property {String} version the component version
+     * @property {String} instanceId the instance id
+     * @property {String} staticId the static id
+     * @property {Object} error the error object obtained when the server-side data detected an error
+     * @property {Object} controller the client-side controller for this component
+     * @property {Array} children the list of instance ids of the direct children
+     * @property {Boolean='false'} controllerLoaded true if the controller file was loaded
+     * @property {Boolean='false'} initEmitted true if the init event was emitted
+     * @property {Boolean='false'} htmlLoaded true if the html content for the component was received
+     */
+    function Component(component) {
+        this.id = component.id;
+        this.version = component.version;
+        this.instanceId = component.instanceId;
 
-                    this.staticId = comp.staticId;
-                    this.controller = null;
-                    this.children = comp.children;
-                }
+        this.staticId = component.staticId;
+        this.error = undefined;
+        this.controller = undefined;
+        this.children = component.children;
 
-                function internalRegister(map, comp) {
-                    var component = new Component(comp);
-                    map[component.instanceId] = component;
+        this.controllerLoaded = false;
+        this.initEmitted = false;
+        this.htmlLoaded = component.htmlLoaded || false;
+    }
 
-                    if (!comp.controller) {
-                        return;
-                    }
-                    require([comp.controller], function (controller) {
-                        for (var key in EventEmitter.prototype) {
-                            controller.prototype.__proto__[key] = EventEmitter.prototype[key];
-                        }
+    /**
+     * Creates a new component registry.
+     *
+     * @name ComponentRegistry
+     * @class Keeps information about the registered components and offers methods for register them.
+     * @constructor
+     */
+    function ComponentRegistry() {}
 
-                        if (typeof controller === 'function') {
-                            controller = new controller();
-                        }
+    /**
+     * Registers a component and invokes the lifecycle.
+     *
+     * @param {Component} component the component to register
+     */
+    ComponentRegistry.prototype.register = function (component) {
+        if (!component || !component.instanceId) {
+            return;
+        }
 
-                        if (typeof controller.init == 'function') {
-                            controller.on('init', controller.init);
-                        }
-
-                        if (typeof controller.start == 'function') {
-                            controller.on('start', controller.start);
-                        }
-
-                        controller.viewContext = new ViewContext(component);
-                        controller.viewContext.find = function (staticId) {
-                            return find(component.instanceId, staticId);
-                        };
-                        component.controller = controller;
-
-                        controller.emit('init');
-                    });
-                }
-
-                function register(comp) {
-                    if (!comp || !comp.instanceId) {
-                        return;
-                    }
-
-                    var component = preComponents[comp.instanceId];
-                    if (component) {
-                        if (component.id != comp.id) {
-                            internalRegister(components, comp);
-                            return;
-                        }
-
-                        component.children = comp.children;
-                        components[comp.instanceId] = component;
-                        if (component.controller) {
-                            component.controller.emit('start');
-                        }
-
-                        delete preComponents[comp.instanceId];
-                    } else {
-                        return internalRegister(components, comp);
-                    }
-                }
-
-                function preRegister(comp) {
-                    if (!comp || !comp.instanceId) {
-                        return;
-                    }
-                    return internalRegister(preComponents, comp);
-                }
-
-                function deregister(instanceId) {
-                    delete components[instanceId];
-                }
-
-                function find(instanceId, staticId) {
-                    var component = components[instanceId];
-                    if (component && component.children) {
-                        var children = component.children;
-                        for (var i = children.length; i--;) {
-                            if (components[children[i]] &&
-                                components[children[i]].staticId == staticId) {
-                                return components[children[i]];
-                            }
-                        }
-                    }
-                }
-
-                return {
-                    preRegister: preRegister,
-                    register: register,
-                    deregister: deregister
-                };
+        var preComponent = preComponents[component.instanceId];
+        if (preComponent) {
+            if (preComponent.id != component.id) {
+                component.htmlLoaded = true;
+                registerComponent(components, component);
+                return;
             }
 
-            return {
-                get: function () {
-                    return _instance || (_instance = getInstance());
-                }
-            };
-        })();
+            preComponent.children = component.children;
+            preComponent.error = component.error;
+            components[preComponent.instanceId] = preComponent;
 
-        return {
-            ComponentRegistry: ComponentRegistry.get()
-        };
-    })();
+            preComponent.htmlLoaded = true;
+            invokeLifecycle(preComponent);
+
+            delete preComponents[preComponent.instanceId];
+        } else {
+            component.htmlLoaded = true;
+            registerComponent(components, component);
+        }
+    };
+
+    /**
+     * Makes a partial registration. This is used to load the client-side controller of the
+     * component before the html comes from the server-side in order to speed up the process
+     * of loading dependencies when the template data takes a longer time to be obtained.
+     *
+     * @param {Component} component the component to register
+     */
+    ComponentRegistry.prototype.preRegister = function (component) {
+        if (!component || !component.instanceId) {
+            return;
+        }
+        return registerComponent(preComponents, component);
+    };
+
+    /**
+     * De-registers a component.
+     *
+     * @param {String} instanceId the component's instance id
+     */
+    ComponentRegistry.prototype.deregister = function (instanceId) {
+        if (instanceId) {
+            delete components[instanceId];
+        }
+    };
+
+    /**
+     * Finds a component by its staticId in the children of a component with the
+     * corresponding instanceId argument.
+     * If the staticId is undefined then all the controllers for the children are returned.
+     *
+     * @param {String} instanceId the component's instance id
+     * @param {String} staticId the child's static id
+     * @returns {Controller|Array|undefined} a controller or a list of controllers
+     * @private
+     * @memberOf ComponentRegistry#
+     */
+    function find(instanceId, staticId) {
+        if (!instanceId) {
+            throw new Error('ComponentRegistry internal error: instanceId must be defined.');
+        }
+        var component = components[instanceId];
+        if (component && component.children) {
+            var children = component.children;
+            if (staticId) {
+                for (var i = children.length; i--;) {
+                    if (components[children[i]] &&
+                        components[children[i]].staticId == staticId) {
+                        return components[children[i]].controller;
+                    }
+                }
+            } else {
+                var controllers = [];
+                for (var i = children.length; i--;) {
+                    if (components[children[i]] && components[children[i]].controller) {
+                        controllers.push(components[children[i]].controller);
+                    }
+                }
+                return controllers;
+            }
+        }
+    }
+
+    /**
+     * Registers a new component and requires the client-side controller.
+     *
+     * @param {Object} map the components map where the new component will be put
+     * @param {Object} component the component properties
+     * @private
+     * @memberOf ComponentRegistry#
+     */
+    function registerComponent(map, component) {
+        var newComponent = new Component(component);
+        map[newComponent.instanceId] = newComponent;
+
+        if (!component.controller) {
+            return;
+        }
+
+        require([component.controller], function (controller) {
+            // Extend the controller with EventEmitter methods.
+            for (var key in EventEmitter.prototype) {
+                controller.prototype[key] = EventEmitter.prototype[key];
+            }
+
+            if (typeof controller === 'function') {
+                controller = new controller();
+            }
+
+            // Bind lifecycle events to methods found in the controller.
+            onControllerEvent(controller, 'init');
+            onControllerEvent(controller, 'error');
+            onControllerEvent(controller, 'start');
+
+            // Attach modules to the controller.
+            controller.context = new ViewContext(newComponent);
+            controller.context.find = function (staticId) {
+                return find(newComponent.instanceId, staticId);
+            };
+
+            newComponent.controller = controller;
+
+            newComponent.controllerLoaded = true;
+            invokeLifecycle(newComponent);
+        });
+    }
+
+    /**
+     * Invokes the component's lifecycle in the following order: init, error, start.
+     *
+     * @param {Object} component the component properties
+     * @private
+     * @memberOf ComponentRegistry#
+     */
+    function invokeLifecycle(component) {
+        if (component.controllerLoaded && !component.initEmitted) {
+            emitControllerEvent(component.controller, 'init');
+            component.initEmitted = true;
+        }
+        if (component.htmlLoaded && component.initEmitted) {
+            if (component.error) {
+                emitControllerEvent(component.controller, 'error', component.error);
+            }
+            emitControllerEvent(component.controller, 'start');
+        }
+    }
+
+    /**
+     * Emits an event from the controller.
+     *
+     * @param {Object} controller the component client-side controller
+     * @param {String} eventName the event name
+     * @param {Object} data the emitted data
+     * @private
+     * @memberOf ComponentRegistry#
+     */
+    function emitControllerEvent(controller, eventName, data) {
+        if (!controller) {
+            return;
+        }
+
+        if (typeof controller[eventName] == 'function') {
+            controller.emit(eventName, data);
+        }
+    }
+
+    /**
+     * Listens for an event.
+     *
+     * @param {Object} controller the component client-side controller
+     * @param {String} eventName the event name
+     * @private
+     * @memberOf ComponentRegistry#
+     */
+    function onControllerEvent(controller, eventName) {
+        if (!controller) {
+            return;
+        }
+
+        if (typeof controller[eventName] == 'function') {
+            controller.on(eventName, controller[eventName]);
+        }
+    }
 
     return raintime;
 });
