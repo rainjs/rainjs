a5d0ce37f626df46c986032127d0b837f8b5791e
Mitko Tschimev <privat@tschimev.de>
2012-03-09 19:38:01 +0200

    added error handling if a view do not exists

--- tests/server/tests/get_latest_version.spec.js
+++ tests/server/tests/get_latest_version.spec.js
@@ -4,25 +4,23 @@ describe('get latest version', function() {
 
     beforeEach(function(){
         require(process.cwd() + '/lib/configuration.js');
-        mockComponentRegistry = loadFile(process.cwd() + '/lib/component_registry.js', {
-            gl: process.cwd() + '/lib/rain_error.js'
-        });
+        mockComponentRegistry = loadFile(process.cwd() + '/lib/component_registry.js');
         mockComponentRegistry.plugins = [];
         componentRegistry = new mockComponentRegistry.ComponentRegistry();
         componentRegistry.initialize();
     });
 
     it('must get the correct version', function() {
-//        expect(componentRegistry.getLatestVersion('example', '0.0.1')).toEqual('0.0.1');
-//        expect(componentRegistry.getLatestVersion('example')).toEqual('4.5.2');
-//        expect(componentRegistry.getLatestVersion('example', '1.3')).toEqual('1.3.5');
-//        expect(componentRegistry.getLatestVersion('example', '1')).toEqual('1.3.5');
-//        expect(componentRegistry.getLatestVersion('example', '2')).toEqual('2.0.1');
+        expect(componentRegistry.getLatestVersion('example', '0.0.1')).toEqual('0.0.1');
+        expect(componentRegistry.getLatestVersion('example')).toEqual('4.5.2');
+        expect(componentRegistry.getLatestVersion('example', '1.3')).toEqual('1.3.5');
+        expect(componentRegistry.getLatestVersion('example', '1')).toEqual('1.3.5');
+        expect(componentRegistry.getLatestVersion('example', '2')).toEqual('2.0.1');
     });
 
     it('must be undefined version', function() {
-//        expect(componentRegistry.getLatestVersion('example', '6')).toBeUndefined();
-//        expect(componentRegistry.getLatestVersion('example_dosnt_exist')).toBeUndefined();
-//        expect(componentRegistry.getLatestVersion('example', '3')).toBeUndefined();
+        expect(componentRegistry.getLatestVersion('example', '6')).toBeUndefined();
+        expect(componentRegistry.getLatestVersion('example_dosnt_exist')).toBeUndefined();
+        expect(componentRegistry.getLatestVersion('example', '3')).toBeUndefined();
     });
 });
