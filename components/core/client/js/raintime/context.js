ffe69b212840a89ed683eb35cc45948298edac34
Mitko Tschimev <privat@tschimev.de>
2012-03-14 00:37:47 +0200

    added messaging to context

--- components/core/client/js/raintime/context.js
+++ components/core/client/js/raintime/context.js
@@ -1,5 +1,5 @@
-define(["core/js/raintime/client_storage"
-], function (ClientStorage) {
+define(["core/js/raintime/client_storage", "core/js/raintime/messaging_observer"],
+       function (ClientStorage, Observer) {
 
     /**
      * The context reflects a component's client-side state. It gives access to other
@@ -17,6 +17,36 @@ define(["core/js/raintime/client_storage"
     function Context(component) {
         this.instanceId = component.instanceId;
         this.storage = new ClientStorage(this);
+        this.messaging = {
+            /**
+             * This is the method that allows registration of a callback method to a
+             * desired event.
+             *
+             * @param {String} eventName Event name we want to subscribe to. Can be any string value.
+             * @param {Function} callback This is the callback method that will get executed. It must have a single parameter called data. e.g.: function(data)
+             */
+            subscribe: function (eventName, callback) {
+                Observer.subscribe(eventName, callback, this);
+            },
+            /**
+             * Unsubscribe from an event.
+             *
+             * @param {String} eventName Event name we want to subscribe to. Can be any string value.
+             * @param {Function} callback This is the callback method that will get executed. It must have a single parameter called data. e.g.: function(data)
+             */
+            unsubscribe: function (eventName, callback) {
+                Observer.unsubscribe(eventName, callback, this);
+            },
+            /**
+             * This is the method that will publish an event and will execute all registered callbacks.
+             *
+             * @param {String} eventName
+             * @param {Object} data
+             */
+            publish: function (eventName, data) {
+                Observer.publish(eventName, data, this);
+            }
+        };
     }
 
     /**
