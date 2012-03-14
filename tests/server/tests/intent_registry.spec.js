8fa3d98daa9ab2824ca7d74cdaa3030bea766abe
Alexandru Bularca <alexandru.bularca@1and1.ro>
2012-03-14 16:36:05 +0200

    added unit tests for intent registry

--- tests/server/tests/intent_registry.spec.js
+++ tests/server/tests/intent_registry.spec.js
@@ -4,6 +4,7 @@ var cwd = process.cwd();
 var loadFile = require(cwd + '/tests/server/rain_mocker');
 var globals = require(cwd + '/lib/globals.js');
 var conf = require(cwd + '/lib/configuration');
+var util = require('util');
 
 var intent = {
     type: 'view',
@@ -20,24 +21,86 @@ var component = {
     }
 };
 
+var intents = {
+    'com.intents.rain.test': {
+        DO_SOMETHING: {
+            'button_2.0':  {
+                type: 'view',
+                provider: {
+                    component: 'button',
+                    version: '2.0',
+                    view: 'some_view'
+                }
+            }
+        }
+    }
+};
+
 describe('Intent Registry', function () {
-    var intentRegistry, intentRegistryContext;
+    var intentRegistry, mockedIntentRegistry;
+    var socketHandlers = {};
 
     beforeEach(function () {
-        /*intentRegistry = loadFile(process.cwd() + '/lib/intent_registry.js', {
-            './configuration': {
-                errorComponent: conf.errorComponent
+        mockedIntentRegistry = loadFile(cwd + '/lib/intent_registry.js', {
+            mocks: {
+                './socket_registry': {
+                    register: function(channel, handler) {
+                        socketHandlers[channel] = handler;
+                    }
+                },
+                './component_registry': {},
+                './renderer': {}
             }
-        }, true);*/
+        }, true);
+        intentRegistry = mockedIntentRegistry.module.exports;
     });
 
-    /*it('must register a intent', function () {
+    it('must register itself to the socket registry on initialization', function () {
+        expect(socketHandlers['/core']).toEqual(mockedIntentRegistry.handleIntent);
+    });
+
+    it('must register a intent', function () {
         intentRegistry.register(component, intent);
 
-        expect(intentRegistryContext['button_2.0']).toBeTruthy();
-    });*/
+        expect(mockedIntentRegistry.intents).toEqual(intents);
+    });
 
-    afterEach(function () {
-        intentRegistryContext = {};
+    it('must throw an error if the intent category is not specified in the descriptor', function () {
+        expect(function () {
+            intentRegistry.register(component, {
+                action: 'some_action',
+                type: 'view',
+                provider: 'some_provider'
+            });
+        }).toThrow('You need to specify a category for an intent in component: ' + component.id + ';' + component.version);
+    });
+
+    it('must throw an error if the intent action is not specified in the descriptor', function () {
+        expect(function () {
+            intentRegistry.register(component, {
+                category: 'some_category',
+                type: 'view',
+                provider: 'some_provider'
+            });
+        }).toThrow('You need to specify a action for an intent in component: ' + component.id + ';' + component.version);
+    });
+
+    it('must throw an error if the intent provider is not specified in the descriptor', function () {
+        expect(function () {
+            intentRegistry.register(component, {
+                category: 'some_category',
+                action: 'some_action',
+                type: 'view'
+            });
+        }).toThrow('You need to specify a provider for an intent in component: ' + component.id + ';' + component.version);
+    });
+
+    it('must throw an error if the intent is already registered', function () {
+        intentRegistry.register(component, intent);
+        expect(function () {
+            intentRegistry.register(component, intent);
+        }).toThrow(['Intent', intent.category, '.', intent.action, 'is already registered.'].join(' '));
+        socketHandlers = {};
     });
 });
+
