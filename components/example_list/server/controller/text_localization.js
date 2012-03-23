9724a58691251f6efae8eb0e204f482dc0b372d5
Mitko Tschimev <privat@tschimev.de>
2012-03-23 18:42:16 +0200

    extended sample application text localization with translation function on server side

---
+++ components/example_list/server/controller/text_localization.js
@@ -0,0 +1,46 @@
+var env = require(process.cwd() + '/lib/environment');
+
+function t(msgid) { return msgid; }
+
+function nt() {}
+
+/**
+ * Resolves the GET HTTP verb.
+ *
+ * @param {Request} request the request object
+ * @param {Response} response the response object
+ */
+function doGet(request, response) {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(t("Email was sent successfully!"));
+}
+
+/**
+ * Resolves the POST HTTP verb.
+ *
+ * @param {Request} request the request object
+ * @param {Response} response the response object
+ */
+function doPost(request, response) {}
+
+/**
+ * Resolves the DELETE HTTP verb.
+ *
+ * @param {Request} request the request object
+ * @param {Response} response the response object
+ */
+function doDelete(request, response) {}
+
+/**
+ * Resolves the PUT HTTP verb.
+ *
+ * @param {Request} request the request object
+ * @param {Response} response the response object
+ */
+function doPut(request, response) {}
+
+exports.get = doGet;
+exports.post = doPost;
+exports['delete'] = doDelete;
+exports.put = doPut;
