1233ec094d11577b37734a62f13f00e2e42b0f0f
Alexandru Bularca <alexandru.bularca@1and1.ro>
2012-03-27 17:33:50 +0300

    fixed a problem with the requireWithContext function

--- tests/server/tests/module.spec.js
+++ tests/server/tests/module.spec.js
@@ -6,17 +6,18 @@ var module = require(cwd + '/lib/module');
 
 describe('Module tests', function () {
 
-    it('must contains the given context in the first level', function () {
+    it('must contain the given context in the first level', function () {
         var lib1 = requireWithContext(cwd + "/tests/server/fixtures/modules/lib1", {
            custom: "context"
         });
         expect(lib1.global.custom).toEqual("context");
     });
 
-    it('must contains the given context in deeper level', function () {
+    it('must contain the given context in deeper level', function () {
         var lib1 = requireWithContext(cwd + "/tests/server/fixtures/modules/lib1", {
             custom: "context"
          });
+
          expect(lib1.global.custom).toEqual("context");
          expect(lib1.lib2.global.custom).toEqual("context");
     });
