ffa1dfdd3f285c23c91974f030866a46ea292173
Alexandru Bularca <alexandru.bularca@1and1.ro>
2012-04-02 08:55:56 +0300

    unit tests for text localization

---
+++ tests/server/tests/text_localization.spec.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var cwd = process.cwd();
+var path = require('path');
+var util = require(path.join(cwd, 'lib', 'util'));
+var componentRegistry = require(path.join(cwd, 'lib', 'component_registry'));
+var translation = require(path.join(cwd, 'lib', 'translation'));
+
+var component = {
+    id: 'example',
+    version: '4.5.2',
+    folder: path.join(cwd, 'tests', 'server', 'fixtures', 'components', 'example_4_5_2')
+}
+
+describe('Text localization', function () {
+    var localeFolder = path.join(component.folder, 'locale');
+
+    util.walkSync(path.join(localeFolder, 'en_US'), ['.po'], function (filePath) {
+        translation.loadLanguageFile(filePath, 'en_US', component);
+    });
+    util.walkSync(path.join(localeFolder, 'ro_RO'), ['.po'], function (filePath) {
+        translation.loadLanguageFile(filePath, 'ro_RO', component);
+    });
+
+    it('must translate if message id exists', function () {
+        expect(translation.translate(component, 'Send email')).toEqual('Trimite email');
+    });
+
+    it('must return the message id, if translation doesn\'t exist', function () {
+        expect(translation.translate(component, 'No translation')).toEqual('No translation');
+    });
+
+    it('must corectly resolve arguments', function () {
+        expect(translation.translate(component, 'Dear %1$s %2$s,', undefined, undefined, ['Jhon', 'Doe'])).toEqual('Bună ziua domnule Doe,');
+    });
+});
