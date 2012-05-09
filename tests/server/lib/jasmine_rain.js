f624671e2c67be13bb0bc071fb8b625ccd0354c6
Daniel Uzunu <daniel.uzunu@1and1.ro>
2012-05-09 16:33:44 +0300

    added mock function and changed the paths to relative ones in order to be able to use jasmine_rain in sprint

--- tests/server/lib/jasmine_rain.js
+++ tests/server/lib/jasmine_rain.js
@@ -1,9 +1,9 @@
 "use strict";
 
 var cwd = process.cwd();
-require(cwd + '/lib/globals');
-
-var loadFile = require(cwd + '/tests/server/rain_mocker');
+//these paths should be relative in order to be able to require this file from sprint
+require('../../../lib/globals');
+var loadFile = require('../rain_mocker');
 
 jasmine.util.extend(jasmine.getGlobal(), (function () {
 
@@ -62,6 +62,64 @@ jasmine.util.extend(jasmine.getGlobal(), (function () {
      */
     JasmineRain.prototype.getModule = function(modulePath) {
         return loadFile(cwd + modulePath, null, true);
+    };
+
+    /**
+     * Mocks the object passed as parameter. All the methods are replaced with spies that return
+     * undefined. The other properties are set to the values specified in props or undefined.
+     *
+     * @param {Object} obj the object to mock
+     * @param {Object} props the value to use for the object's property values when mocking
+     */
+    JasmineRain.prototype.mock = function(obj, props) {
+        var spec = jasmine.getEnv().currentSpec;
+
+        //create the mocks array the first time mock is called for the current spec
+        if (!spec.__mocks) {
+            spec.__mocks = [];
+        }
+
+        var saveInitialValues = true;
+
+        //if mock already executed for this object don't override the initial values
+        if (!obj.__initialValues) {
+            obj.__initialValues = {};
+            spec.__mocks.push(obj);
+        } else {
+            saveInitialValues = false;
+        }
+
+        for (var key in obj) {
+            if (key === '__initialValues') {
+                continue;
+            }
+            if (typeof obj[key] === 'function') {
+                spyOn(obj, key);
+            } else {
+                if (saveInitialValues) {
+                    obj.__initialValues[key] = obj[key];
+                }
+                obj[key] = props ? props[key] : undefined;
+            }
+        }
+    };
+
+    var oldFinish = jasmine.Spec.prototype.finish;
+
+    //override the finish method to restore the mocked object to its initial state
+    //spies are already restored by jasmine
+    jasmine.Spec.prototype.finish = function (onComplete) {
+        oldFinish.call(this, onComplete);
+
+        if (this.__mocks) {
+            for (var i = 0, len = this.__mocks.length; i < len; i++) {
+                var obj = this.__mocks[i];
+                for (var key in obj.__initialValues) {
+                    obj[key] = obj.__initialValues[key];
+                }
+                delete obj.__initialValues;
+            }
+        }
     };
 
     return new JasmineRain();
