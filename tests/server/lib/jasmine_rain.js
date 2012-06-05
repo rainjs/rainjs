da13248f489320835ee3ed68ec0d69312a673dba
Daniel Uzunu <daniel.uzunu@1and1.ro>
2012-06-05 12:23:16 +0300

    removed mock method from jasmine rain

--- tests/server/lib/jasmine_rain.js
+++ tests/server/lib/jasmine_rain.js
@@ -180,64 +180,6 @@ jasmine.util.extend(jasmine.getGlobal(), (function () {
         return require(mod);
     }
 
-    /**
-     * Mocks the object passed as parameter. All the methods are replaced with spies that return
-     * undefined. The other properties are set to the values specified in props or undefined.
-     *
-     * @param {Object} obj the object to mock
-     * @param {Object} props the value to use for the object's property values when mocking
-     */
-    JasmineRain.prototype.mock = function(obj, props) {
-        var spec = jasmine.getEnv().currentSpec;
-
-        //create the mocks array the first time mock is called for the current spec
-        if (!spec.__mocks) {
-            spec.__mocks = [];
-        }
-
-        var saveInitialValues = true;
-
-        //if mock already executed for this object don't override the initial values
-        if (!obj.__initialValues) {
-            obj.__initialValues = {};
-            spec.__mocks.push(obj);
-        } else {
-            saveInitialValues = false;
-        }
-
-        for (var key in obj) {
-            if (key === '__initialValues') {
-                continue;
-            }
-            if (typeof obj[key] === 'function') {
-                spyOn(obj, key);
-            } else {
-                if (saveInitialValues) {
-                    obj.__initialValues[key] = obj[key];
-                }
-                obj[key] = props ? props[key] : obj[key];
-            }
-        }
-    };
-
-    var oldFinish = jasmine.Spec.prototype.finish;
-
-    //override the finish method to restore the mocked object to its initial state
-    //spies are already restored by jasmine
-    jasmine.Spec.prototype.finish = function (onComplete) {
-        oldFinish.call(this, onComplete);
-
-        if (this.__mocks) {
-            for (var i = 0, len = this.__mocks.length; i < len; i++) {
-                var obj = this.__mocks[i];
-                for (var key in obj.__initialValues) {
-                    obj[key] = obj.__initialValues[key];
-                }
-                delete obj.__initialValues;
-            }
-        }
-    };
-
     return new JasmineRain();
 }()));
 
