6a8c07c084e3d0b18ba999b896fd7b120f57f20a
Daniel Uzunu <daniel.uzunu@1and1.ro>
2012-06-14 15:58:36 +0300

    updated unit test for data layer

--- tests/server/tests/data_layer.spec.js
+++ tests/server/tests/data_layer.spec.js
@@ -25,112 +25,147 @@
 
 "use strict";
 
-var cwd = process.cwd();
-var globals = require(cwd + '/lib/globals.js');
-var config = require(cwd + '/lib/configuration.js');
+var path = require('path'),
+    environment = require(process.cwd() + '/lib/environment');
 
 describe('Data layer', function() {
-    var mockComponentRegistry, componentRegistry,
-        mockDataLayer, dataLayer;
-
-    var error, context;
+    var dataLayer, cb, fn, componentOpt;
 
     beforeEach(function () {
-        mockComponentRegistry = loadModuleContext('/lib/component_registry.js');
-        mockComponentRegistry.scanComponentFolder();
-        componentRegistry = new mockComponentRegistry.ComponentRegistry();
-
-        var mocks = {
-            './component_registry': componentRegistry
-        };
-
-        mockDataLayer = loadModuleContext('/lib/data_layer.js', mocks);
-
-        dataLayer = new mockDataLayer.DataLayer();
-    });
-
-    function saveParameters(callbackError, callbackContext) {
-        error = callbackError;
-        context = callbackContext;
-    }
-
-    it('must throw an error when required arguments are missing or invalid', function () {
-        var componentOpt = undefined;
-        var finished = false;
-
-        runs(function () {
-            dataLayer.loadData(componentOpt, saveParameters);
-            expect(error.message).toBe('Missing componentOptions in function loadData().');
-
-            componentOpt = {};
-            dataLayer.loadData(componentOpt, saveParameters);
-            expect(error.message).toBe('Missing component id in function loadData().');
-
-            componentOpt.id = 'button';
-            dataLayer.loadData(componentOpt, saveParameters);
-            expect(error.message).toBe('Missing view id in function loadData().');
-
-            componentOpt.viewId = 'index';
-            dataLayer.loadData(componentOpt, saveParameters);
-            expect(error.message).toBe('Missing version in function loadData().');
-
-            componentOpt.version = '1.0';
-            expect(function () {
-                dataLayer.loadData(componentOpt);
-            }).toThrow('Missing callback in function loadData().');
-
-            componentOpt.id = 'inexistent';
-            dataLayer.loadData(componentOpt, saveParameters);
-            expect(error.message).toBe('Component inexistent-1.0 doesn\'t exist.');
-
-            componentOpt.id = 'button';
-            componentOpt.viewId = 'no_view';
-            dataLayer.loadData(componentOpt, saveParameters);
-            expect(error.message).toBe('View no_view doesn\'t exists in meta.json.');
-
-            componentOpt.viewId = 'index';
-            componentOpt.context = 'my_data';
-            componentRegistry.getConfig('button', '1.0').folder = 'path';
-            dataLayer.loadData(componentOpt, function () {
-                saveParameters(arguments[0], arguments[1]);
-                finished = true;
-            });
-        });
-
-        waitsFor(function () {
-            return finished;
-        }, 'Callback was\'t called.');
-
-        runs(function () {
-            expect(error).toBeNull();
-            expect(context).toBe('my_data');
-        });
-    });
-
-    it('must call the server-side data function for the view', function () {
-        var componentOpt = {
+        componentOpt = {
             id: 'button',
             version: '1.0',
-            viewId: 'level3',
-            context: 'my_data'
+            viewId: 'index',
+            context: 'my_data',
+            session: {key: 'value'},
+            request: {
+                query: {page: 1},
+                url: '/example/index',
+                headers: {}
+            }
         };
-        var finished = false;
 
-        runs(function () {
-            dataLayer.loadData(componentOpt, function () {
-                saveParameters(arguments[0], arguments[1]);
-                finished = true;
+        cb = jasmine.createSpy();
+        fn = jasmine.createSpy().andCallFake(function (environment, callback, context, request) {
+            callback(null, {oldData: context, newData: 'my_new_data'});
+        });
+
+        spyOn(global, 'requireWithContext').andCallFake(function () {
+            return {
+                index: fn
+            };
+        });
+
+        var mocks = {
+            './component_registry': jasmine.createSpyObj('component_registry', ['getConfig']),
+            path: jasmine.createSpyObj('component_registry', ['exists', 'join'])
+        };
+        dataLayer = loadModuleExports('/lib/data_layer.js', mocks);
+
+        mocks.path.exists.andCallFake(function (path, callback) {
+            callback(true);
+        });
+        mocks.path.join.andCallFake(path.join);
+        mocks['./component_registry'].getConfig.andCallFake(function (id, version) {
+            if (id === 'button' && version === '1.0') {
+                return {
+                    views: {
+                        index: {}
+                    },
+                    folder: 'button'
+                };
+            }
+        });
+    });
+
+    describe('loadData', function () {
+        beforeEach(function () {
+            spyOn(dataLayer, '_createCustomRequest').andReturn({session: 'data'});
+        });
+
+        it('should throw an error when required arguments are missing or invalid', function () {
+            dataLayer.loadData(undefined, cb);
+            expect(cb.mostRecentCall.args[0].message)
+                .toBe('Missing componentOptions in function loadData().');
+
+            dataLayer.loadData({}, cb);
+            expect(cb.mostRecentCall.args[0].message).toBe('Missing component id in function loadData().');
+
+            dataLayer.loadData({id: 'button'}, cb);
+            expect(cb.mostRecentCall.args[0].message).toBe('Missing view id in function loadData().');
+
+            dataLayer.loadData({id: 'button', viewId: 'index'}, cb);
+            expect(cb.mostRecentCall.args[0].message).toBe('Missing version in function loadData().');
+
+            expect(function () {
+                dataLayer.loadData({id: 'button', viewId: 'index', version: '1.0'});
+            }).toThrow('Missing callback in function loadData().');
+
+            dataLayer.loadData({id: 'inexistent', viewId: 'index', version: '1.0'}, cb);
+            expect(cb.mostRecentCall.args[0].message).toBe('Component inexistent-1.0 doesn\'t exist.');
+
+            dataLayer.loadData({id: 'button', viewId: 'no_view', version: '1.0'}, cb);
+            expect(cb.mostRecentCall.args[0].message).toBe('View no_view doesn\'t exists in meta.json.');
+        });
+
+        it('should call the server-side data function for the view', function () {
+            runs(function () {
+                dataLayer.loadData(componentOpt, cb);
+            });
+
+            waitsFor(function () {
+                return cb.calls.length !== 0;
+            }, 'callback to be called.');
+
+            runs(function () {
+                expect(requireWithContext.mostRecentCall.args[0])
+                    .toEqual(path.join('button', 'server/data.js'));
+                expect(cb.mostRecentCall.args[0]).toBeNull();
+                expect(cb.mostRecentCall.args[1].oldData).toBe('my_data');
+                expect(cb.mostRecentCall.args[1].newData).toBe('my_new_data');
             });
         });
 
-        waitsFor(function () {
-            return finished;
-        }, 'Callback was\'t called.');
+        it('should pass 4 parameters to the data layer method', function () {
+            runs(function () {
+                dataLayer.loadData(componentOpt, cb);
+            });
 
-        runs(function () {
-            expect(error).toBeNull();
-            expect(context.old_data).toBe('my_data');
-            expect(context.new_data).toBe('my_new_data');
+            waitsFor(function () {
+                return cb.calls.length !== 0;
+            }, 'callback to be called.');
+
+            runs(function () {
+                var args = fn.mostRecentCall.args;
+                expect(args.length).toEqual(4);
+                expect(args[0]).toBe(environment);
+                expect(typeof args[1]).toEqual('function');
+                expect(args[2]).toEqual(componentOpt.context);
+                expect(args[3]).toEqual({session: 'data'});
+            });
+        });
+    });
+
+    describe('_createCustomRequest', function () {
+        it('should create a custom request object (HTTP case)', function () {
+            var req = dataLayer._createCustomRequest(componentOpt);
+
+            expect(req.session).toBe(componentOpt.session);
+            expect(req.query).toBe(componentOpt.request.query);
+            expect(req.headers).toBe(componentOpt.request.headers);
+            expect(req.url).toBe(componentOpt.request.url);
+            expect(req.type).toEqual('HTTP');
+        });
+
+        it('should create a custom request object (WebSocket case)', function () {
+            delete componentOpt.request;
+            var req = dataLayer._createCustomRequest(componentOpt);
+
+            expect(req.session).toBe(componentOpt.session);;
+            expect(req.query).toBeUndefined();
+            expect(req.headers).toBeUndefined();
+            expect(req.url).toBeUndefined();
+            expect(req.type).toEqual('WebSocket');
         });
     });
 });
