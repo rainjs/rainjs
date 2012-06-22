bded70cd617be29ffc411de7ae200fe6eb902eb2
Alexandru Bularca <alexandru.bularca@1and1.ro>
2012-06-22 15:37:31 +0300

    fixed a crash in a test

--- tests/server/tests/raind.spec.js
+++ tests/server/tests/raind.spec.js
@@ -30,13 +30,14 @@ describe('raind', function () {
     });
 
     it('should correctly setup the command line options', function () {
-        loadModuleExports(path.join('bin', 'raind'), mocks);
+        var raind = loadModuleContext(path.join('bin', 'raind'), mocks);
 
         expect(program.version).toHaveBeenCalled();
         expect(program.option.calls.length).toBe(3);
         expect(program.option.calls[0].args).toEqual([
             '-c, --conf <path>',
             'start the server with a custom configuration file',
+            raind.getProjectRoot,
             path.join(process.cwd(), 'conf', 'server.conf.default')
         ]);
         expect(program.option.calls[1].args).toEqual([
