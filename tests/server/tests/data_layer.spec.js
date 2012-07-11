23f91b0585181f532ae524e7a7ea1d57c4cacd82
Ciprian Roman <roudolph@gmail.com>
2012-07-11 09:04:44 +0300

    Pass the user session to the environment object.

--- tests/server/tests/data_layer.spec.js
+++ tests/server/tests/data_layer.spec.js
@@ -26,7 +26,7 @@
 "use strict";
 
 var path = require('path'),
-    environment = require(process.cwd() + '/lib/environment');
+    Environment = require(process.cwd() + '/lib/environment');
 
 describe('Data layer', function() {
     var dataLayer, cb, fn, componentOpt;
@@ -139,7 +139,7 @@ describe('Data layer', function() {
             runs(function () {
                 var args = fn.mostRecentCall.args;
                 expect(args.length).toEqual(4);
-                expect(args[0]).toBe(environment);
+                expect(args[0]).toEqual(new Environment());
                 expect(typeof args[1]).toEqual('function');
                 expect(args[2]).toEqual(componentOpt.context);
                 expect(args[3]).toEqual({session: 'data'});
