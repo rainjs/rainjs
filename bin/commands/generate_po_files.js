2f14ef2236167a9bda81a1398ed9e05a4dbddf9b
Ciprian Roman <roudolph@gmail.com>
2012-07-18 12:25:42 +0300

    Added logic for parsing the template files and extracting the translation messages.

--- bin/commands/generate_po_files.js
+++ bin/commands/generate_po_files.js
@@ -28,7 +28,9 @@
 var path = require('path'),
     fs = require('fs'),
     color = require('colors'),
-    utils = require('../lib/utils');
+    utils = require('../lib/utils'),
+    extend = require('node.extend'),
+    util = require('../../lib/util');
 
 /**
  * Register the generate localization files command.
@@ -51,7 +53,141 @@ function register(program) {
  * @param {String} [componentId] the component identifier (id;version)
  */
 function generateLocalizationFiles(sourceLocale, outputLocales, componentId) {
-    
+    try {
+        var projectRoot = utils.getProjectRoot(process.cwd()),
+            componentsFolder = path.join(projectRoot, 'components'),
+            components = scanComponents(componentsFolder);
+
+        for (var i = components.length; i--;) {
+            var module = components[i].id + ';' + components[i].version;
+            if (componentId && module != componentId) {
+                continue;
+            }
+
+            try {
+                var translations = parseComponent(components[i]);
+                console.log(JSON.stringify(translations));
+            } catch (ex) {
+                console.log(ex.message.red);
+            }
+        }
+    } catch (ex) {
+        console.log(ex.message.red);
+        return;
+    }
+}
+
+/**
+ * Scan the components folder and read the meta file.
+ *
+ * @param {String} componentsFolder the components folder
+ * @returns {Array}
+ */
+function scanComponents(componentsFolder) {
+    var components = [],
+        folders;
+    try {
+        folders = fs.readdirSync(componentsFolder);
+    } catch (ex) {
+        throw new RainError('The components folder: ' + componentsFolder + ' does not exist!',
+            RainError.ERROR_IO);
+    }
+
+    for (var i = 0, len = folders.length; i < len; i++) {
+        var componentPath = path.join(componentsFolder, folders[i]);
+        try {
+            if (!fs.statSync(componentPath).isDirectory()) {
+                continue;
+            }
+        } catch (ex) {
+            console.log(('Failed to call stat for ' + componentPath).red);
+            break;
+        }
+
+        try {
+            var metaFile = path.join(componentPath, 'meta.json');
+            var config = JSON.parse(fs.readFileSync(metaFile, 'utf8'));
+            config.folder = componentPath;
+            components.push(config);
+        } catch (ex) {
+            console.log(('Failed to load meta.json from ' + componentPath + ' folder!').red);
+        }
+    }
+
+    return components;
+}
+
+/**
+ * Parse a component's files and extract the translations.
+ *
+ * @param {Object} component the component configuration
+ * @returns {Object} the component's translations found in templates and .js files
+ */
+function parseComponent(component) {
+    var translations = {};
+
+    extend(translations, parseTemplateFiles(component));
+    extend(translations, parseJsFiles(component));
+
+    return translations;
+}
+
+/**
+ * Extract the translations from a component's templates.
+ *
+ * @param {Object} component the component configuration
+ * @returns {Object} the translation messages indexed by the file path
+ */
+function parseTemplateFiles(component) {
+    var templatesFolder = path.join(component.folder, 'client/templates'),
+        templateTranslations = {};
+
+    util.walkSync(templatesFolder, ['.html'], function (filePath) {
+        try {
+            var matchString = '(((\\\\")?[^\\\\"]+(\\\\[^"])?(\\\\")?)+)',
+                tRegExp = new RegExp('(?:{{t\\s+")' + matchString + '(?:")', 'gm'),
+                ntRegExp = new RegExp('(?:{{nt\\s+")' + matchString + '(?:"\\s+")' +
+                                                        matchString + '(?:")', 'gm'),
+                text = fs.readFileSync(filePath, 'utf8'),
+                matches,
+                translations = [];
+
+            while ((matches = tRegExp.exec(text)) != null) {
+                if (matches.length < 2 || !matches[1]) {
+                    continue;
+                }
+
+                translations.push(matches[1]);
+            }
+
+            while ((matches = ntRegExp.exec(text)) != null) {
+                if (matches.length < 7 || !matches[1] || !matches[6]) {
+                    continue;
+                }
+
+                translations.push([matches[1], matches[6]]);
+            }
+
+            if (translations.length > 0) {
+                templateTranslations[filePath] = translations;
+            }
+        } catch (ex) {
+            console.log(('Could not extract the template translations from ' + filePath).red,
+                        ex.message);
+        }
+    });
+
+    return templateTranslations;
+}
+
+/**
+ * Extract the translations from a component's .js files.
+ *
+ * @param {Object} component the component configuration
+ * @returns {Object} the translation messages indexed by the file path
+ */
+function parseJsFiles(component) {
+    return {};
 }
 
 module.exports = register;
