629b07bdf729c17862cfbe3489a842a87b68df88
Răzvan Mocanu <razvan.mocanu@1and1.ro>
2012-07-20 14:19:21 +0300

    generate po utils compare translations unit test
    contains general skeleton for unit tests for this module

--- tests/server/tests/sdk/generate_po_utils.spec.js
+++ tests/server/tests/sdk/generate_po_utils.spec.js
@@ -1,3 +1,32 @@
 describe('generate po utils', function () {
 
+    var GeneratePoUtils, utils;
+
+    beforeEach(function () {
+        GeneratePoUtils = loadModuleExports('bin/lib/generate_po_utils.js');
+
+        utils = new GeneratePoUtils();
+    });
+
+    describe('merge translations', function () {
+        describe('compareTranslations', function () {
+            var component, po, parsed;
+
+            beforeEach(function () {
+                spyOn(utils, 'updateExistingTranslations');
+                spyOn(utils, 'addNewTranslations');
+            });
+
+            it('should merge translations', function () {
+                component = {};
+                po = {};
+                parsed = {};
+
+                utils.compareTranslations(component, po, parsed);
+
+                expect(utils.updateExistingTranslations).toHaveBeenCalledWith(po, parsed);
+                expect(utils.addNewTranslations).toHaveBeenCalledWith(component, po, parsed);
+            });
+        });
+    });
 });
