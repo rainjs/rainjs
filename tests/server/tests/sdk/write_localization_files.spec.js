69564cebe113cd654f8316ea1d87083902373f31
Alexandru Bularca <alexandru.bularca@1and1.ro>
2012-07-23 11:15:16 +0300

    added unit tests for localization file creation

--- tests/server/tests/sdk/write_localization_files.spec.js
+++ tests/server/tests/sdk/write_localization_files.spec.js
@@ -1,5 +1,5 @@
 describe("Localization files", function() {
-    var GeneratePoUtils,
+    var GeneratePoUtils, fs, wrench, mocks = {},
         locale = {
             "": {
                 header1: 'header 1',
@@ -10,7 +10,10 @@ describe("Localization files", function() {
         };
 
     beforeEach(function () {
-        GeneratePoUtils = loadModuleExports('/bin/lib/generate_po_utils.js');
+        mocks['fs'] = fs = jasmine.createSpyObj('fs', ['writeFileSync']);
+        mocks['wrench'] = wrench = jasmine.createSpyObj('wrench', ['mkdirSyncRecursive']);
+
+        GeneratePoUtils = loadModuleExports('/bin/lib/generate_po_utils.js', mocks);
     });
 
     it("should corectly generate the translation", function() {
@@ -30,5 +33,83 @@ describe("Localization files", function() {
             'msgstr[0] "Some other message"\n' +
             'msgstr[1] "Plural message"\n'
         );
+    });
+
+    it('should try to write the translation to the file', function () {
+        var translation = new GeneratePoUtils();
+        var generatedText =
+            'msgid ""\n' +
+            'msgstr ""\n' +
+            '"header1: header 1"\n' +
+            '"header2: header 2"\n\n' +
+
+            'msgid "Some translation"\n' +
+            'msgstr "Some translation"\n\n' +
+
+            'msgid "Some other message"\n' +
+            'msgid_plural "Plural id"\n' +
+            'msgstr[0] "Some other message"\n' +
+            'msgstr[1] "Plural message"\n';
+
+        var component = {
+            id: 'component',
+            version: '1.0',
+            views: { index: {} },
+            folder: '/component/path'
+        };
+
+        var poTranslations = {
+            '/some/path': {
+                '': {
+                    'Content-Type': 'text/plain; charset=UTF-8\\n',
+                    'Plural-Forms': 'nplurals=2; plural=(n != 1);\\n'
+                },
+                'Some message': [ null, 'Some message' ]
+            }
+        };
+
+        spyOn(translation, 'composePoContent').andReturn(generatedText);
+
+        translation.createPoFiles(component, poTranslations);
+
+        expect(fs.writeFileSync).toHaveBeenCalledWith('/some/path', generatedText, 'utf8');
+    });
+
+    it('shouldn\'t do anything if the file doesn\'t have any messages', function () {
+        var translation = new GeneratePoUtils();
+        var generatedText =
+            'msgid ""\n' +
+            'msgstr ""\n' +
+            '"header1: header 1"\n' +
+            '"header2: header 2"\n\n' +
+
+            'msgid "Some translation"\n' +
+            'msgstr "Some translation"\n\n' +
+
+            'msgid "Some other message"\n' +
+            'msgid_plural "Plural id"\n' +
+            'msgstr[0] "Some other message"\n' +
+            'msgstr[1] "Plural message"\n';
+
+        var component = {
+            id: 'component',
+            version: '1.0',
+            views: { index: {} },
+            folder: '/component/path'
+        };
+
+        var poTranslations = {
+            '/some/path': {
+                '': {
+                    'Content-Type': 'text/plain; charset=UTF-8\\n',
+                    'Plural-Forms': 'nplurals=2; plural=(n != 1);\\n'
+                }
+            }
+        };
+
+        spyOn(translation, 'composePoContent').andReturn(generatedText);
+        translation.createPoFiles(component, poTranslations);
+
+        expect(fs.writeFileSync).not.toHaveBeenCalled();
     });
 });
