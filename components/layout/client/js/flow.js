81964c911483be96c89c90d2f2ade7b1adf2a3c7
Alexandru Bularca <alexandru.bularca@1and1.ro>
2012-08-01 16:03:27 +0300

    added flow layout tests

--- components/layout/client/js/flow.js
+++ components/layout/client/js/flow.js
@@ -1,4 +1,5 @@
 define(['util', 'layout/1.0/js/layout'], function (Util, Layout) {
+    "use strict";
 
     /**
      * This is the client-side implementation of the flow layout.
@@ -23,61 +24,47 @@ define(['util', 'layout/1.0/js/layout'], function (Util, Layout) {
 
         this._container = root.find('.flow-container');
         this._items = this._container.find('.item');
-
-        this.orientation = this._container.attr('data-orientation') || 'ltr';
     };
 
     /**
      * Create a new item placeholder.
      *
      * @param {Object} options parameters needed to know how to configure the place for the component
-     * @param {Number} index the index where the new item will be placed
+     * @param {Number} [options.index] the index where the new item will be placed
      * @throws {RainError} if start wasn't executed when the method was called
      * @returns {jQueryObject} the element where the new component can be inserted
      */
     FlowLayout.prototype._createNewItem = function (options) {
+        options = options || {};
         if (!this._container) {
             throw new RainError('API methods cannot be called before start is executed',
                                 RainError.ERROR_PRECONDITION_FAILED);
         }
 
-        var index = options.index;
-        if (typeof index === 'undefined' || index < 0 || index > this._items.length) {
-            if (this.orientation == 'ltr') {
-                index = this._items.length;
-            } else {
-                index = 0;
-            }
-        }
+        var item = $('<div class="item"></div>');
+        this._container.append(item);
+        this._items.push(item);
 
-        var item = '<div class="item"></div>';
-        if (this._items.length == 0) {
-            this._container.html(item);
-        } else if (index == 0) {
-            $(item).insertBefore(this._items[0]);
-        } else {
-            $(item).insertAfter(this._items[index - 1]);
-        }
-
-        this._items = this._container.find('.item');
-
-        return $(this._items[index]);
+        return item;
     };
 
     /**
      * Remove a component from the layout.
      *
      * @param {Object} options parameters to identify what will be removed
+     * @param {Object} options.index the index to remove
      * @throws {RainError} if start wasn't executed when the method was called
      */
     FlowLayout.prototype._remove = function (options) {
+        options = options || {};
+
         if (!this._container) {
             throw new RainError('API methods cannot be called before start is executed',
                                 RainError.ERROR_PRECONDITION_FAILED);
         }
 
         var index = options.index;
-        if (typeof index === 'undefined') {
+        if ('undefined' === typeof index) {
             throw new RainError('The "index" parameter is missing.',
                                 RainError.ERROR_PRECONDITION_FAILED);
         }
