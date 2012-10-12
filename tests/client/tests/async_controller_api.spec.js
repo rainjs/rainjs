719b97bc45150f6f809cf0499e08b05990cd0ec4
Daniel Uzunu <daniel.uzunu@1and1.ro>
2012-10-12 14:00:52 +0300

    formatted async controller unit tests

--- tests/client/tests/async_controller_api.spec.js
+++ tests/client/tests/async_controller_api.spec.js
@@ -24,29 +24,23 @@
 // IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 describe('AsyncController API', function () {
-    var Mocks, ac;
-
-    beforeEach(function () {
-        Mocks = {};
-        ac = null;
-    });
-
-
     describe('Get Child', function () {
         it('should get a child', ['raintime/async_controller'],
            function (AsyncController) {
                 AsyncController.prototype._getChild.andCallThrough();
+
                 var fakeController = jasmine.createSpyObj('fakeController', ['on']);
                 fakeController.on.andCallFake(function (event, handler) {
                     handler();
                 });
-                ac = new AsyncController();
+
+                var ac = new AsyncController();
                 ac.context = {};
                 ac.context.find = jasmine.createSpy();
                 ac.context.find.andCallFake(function (sid, fn) {
                     fn.apply(fakeController);
                 });
-                ac._controllers['#1'] = fakeController;
+
                 var resolvedController;
                 var sid = "123";
                 ac._getChild(sid).then(function (controller) {
@@ -55,117 +49,137 @@ describe('AsyncController API', function () {
 
                 expect(resolvedController).toEqual(fakeController);
                 expect(ac.context.find).toHaveBeenCalledWith(sid, jasmine.any(Function));
-                expect(fakeController.on).toHaveBeenCalledWith('start',jasmine.any(Function));
+                expect(fakeController.on).toHaveBeenCalledWith('start', jasmine.any(Function));
 
            }
         );
+
         it ('should get the child from the cache', ['raintime/async_controller' , 'util'],
-                function(AsyncController,Util){
-                    AsyncController.prototype._getChild.andCallThrough();
-                    Util.defer.andCallThrough();
-                    var fakeController = {'on' : 'start'};
-                    ac = new AsyncController();
-                    ac.context = {};
-                    ac.context.find = jasmine.createSpy();
-                    var resolvedController;
-                    ac._controllers['#1'] = fakeController;
-                    var isFinished = false;
-                    runs(function () {
-                        ac._getChild('#1').then(function (controller) {
-                            resolvedController = controller;
+            function(AsyncController,Util){
+                AsyncController.prototype._getChild.andCallThrough();
+                Util.defer.andCallThrough();
+
+                var fakeController = {'on' : 'start'};
+
+                var ac = new AsyncController();
+                ac.context = {};
+                ac.context.find = jasmine.createSpy();
+                ac._controllers['sid1'] = fakeController;
+
+                var isFinished = false,
+                    resolvedController;
+
+                runs(function () {
+                    ac._getChild('sid1').then(function (controller) {
+                        resolvedController = controller;
+                        isFinished = true;
+                    });
+                });
+
+                waitsFor(function(){
+                    return isFinished;
+                }, 'service to be retrieved');
+
+                runs(function () {
+                    expect(ac.context.find).not.toHaveBeenCalled();
+                    expect(resolvedController).toEqual(fakeController);
+                });
+            }
+        );
+
+        it ('should get an error if no sid found',
+            ['raintime/async_controller', 'util', 'raintime/lib/rain_error'],
+            function(AsyncController,Util,rain_error){
+                AsyncController.prototype._getChild.andCallThrough();
+                Util.defer.andCallThrough();
+
+                var ac = new AsyncController();
+                ac.context = {};
+                ac.context.find = jasmine.createSpy();
+                ac.context.find.andCallFake(function (sid){
+                    return [sid];
+                });
+
+                var isFinished = false,
+                    sid = '123',
+                    error;
+
+                runs(function () {
+                    ac._getChild(sid).then(
+                        function () {},
+                        function (err) {
+                            error = err;
                             isFinished = true;
-                        });
-                    });
-                    waitsFor(function(){
-                        return isFinished;
-                    },'service to be retrieved');
-                    runs(function () {
-                        expect(ac.context.find).not.toHaveBeenCalled();
-                        expect(resolvedController).toEqual(fakeController);
-                    });
-        });
-        it ('should get an error if no sid found', ['raintime/async_controller', 'util', 'raintime/lib/rain_error'],
-                function(AsyncController,Util,rain_error){
-                    AsyncController.prototype._getChild.andCallThrough();
-                    Util.defer.andCallThrough();
-                    var sid = "123"; 
-                    ac = new AsyncController();
-                    ac.context = {};
-                    ac.context.find = jasmine.createSpy();
-                    ac.context.find.andCallFake(function (sid){
-                        return [sid];
-                    });
-                    var error;
-                    var isFinished = false;
-                    runs(function () {
-                        ac._getChild(sid).then(
-                                function () {},
-                                function (err) {
-                                    error = err;
-                                    isFinished = true;
-                                }
-                        );
-                    });
-                    waitsFor(function(){
-                        return isFinished;
-                    },'service to be retrieved');
-                    runs(function () {
-                        expect(ac.context.find).toHaveBeenCalledWith(sid, jasmine.any(Function));
-                        expect(error).toBeDefined();
-                    });
-        });
+                        }
+                    );
+                });
+
+                waitsFor(function(){
+                    return isFinished;
+                }, 'service to be retrieved');
+
+                runs(function () {
+                    expect(ac.context.find).toHaveBeenCalledWith(sid, jasmine.any(Function));
+                    expect(error).toBeDefined();
+                });
+            }
+        );
     });
 
     describe('_onChild', function () {
         it('should step through all the phases', ['raintime/async_controller'],
            function (AsyncController) {
                 AsyncController.prototype._onChild.andCallThrough();
-                ac = new AsyncController();
-                var sid = "123";
-                var event = "click";
-                var isDone = false;
-                var fakeController = jasmine.createSpyObj('fakeController', ['on']);
+
+                var sid = "123",
+                    event = "click",
+                    isFinished = false,
+                    fakeController = jasmine.createSpyObj('fakeController', ['on']),
+                    ac = new AsyncController();
+
                 fakeController.on.andCallFake(function (event, handler) {
                     handler();
-                    isDone = true;
                 });
-                ac.context = {};
-                ac._getChild = jasmine.createSpy();
-                ac._getChild.andCallFake(function () {
-                    isFinished = true;
+
+                ac._getChild.andCallFake(function (sid) {
                     return fakeController;
                 });
-                var isFinished = false;
 
                 runs(function () {
-                    ac._onChild(sid,event,function () {});
+                    ac._onChild(sid, event, function () {
+                        isFinished = true;
+                    });
                 });
+
                 waitsFor(function() {
                     return isFinished;
                 },'service to be retrieved');
+
                 runs(function () {
-                    expect(ac._onChild).toHaveBeenCalledWith(sid,event,jasmine.any(Function));
+                    expect(fakeController.on).toHaveBeenCalledWith(event, jasmine.any(Function));
                     expect(ac._getChild).toHaveBeenCalledWith(sid);
                 });
-                waitsFor(function() {
-                    return isDone;
-                },'service to be retrieved');
-                runs(function () {
-                    expect(fakeController.on).toHaveBeenCalledWith(event,jasmine.any(Function));
-                });
-           }
+            }
         );
     });
 
     describe('_getChildren', function () {
-        it('should call getChild() with sid', ['raintime/async_controller', 'raintime/lib/promise' , 'util'],
-           function (AsyncController, promise, util) {
+        it('should call getChild() with sid',
+            ['raintime/async_controller', 'raintime/lib/promise' , 'util'],
+            function (AsyncController, promise, util) {
                 AsyncController.prototype._getChildren.andCallThrough();
                 util.defer.andCallThrough();
-                var fakeController1 = jasmine.createSpyObj('fakeController', ['on']);
-                var fakeController2 = jasmine.createSpyObj('fakeController', ['on']);
-                ac = new AsyncController();
-                ac._getChild = jasmine.createSpy();
+
+                var fakeController1 = jasmine.createSpyObj('fakeController', ['on']),
+                    fakeController2 = jasmine.createSpyObj('fakeController', ['on']),
+                    ac = new AsyncController(),
+                    isFinished = false,
+                    resolvedObject,
+                    expecting = {
+                        sid1 : fakeController1,
+                        sid2 : fakeController2
+                    };
+
                 ac._getChild.andCallFake(function (sid) {
                     var deferred = promise.defer();
                     if (sid === 'sid1'){
@@ -176,23 +190,17 @@ describe('AsyncController API', function () {
                     }
                     return deferred.promise;
                 });
-                var sid = "123";
-                var isFinished = false;
-                var resolvedObject;
-                var expecting = {
-                        sid1 : fakeController1,
-                        sid2 : fakeController2
-                };
-                ac.context = {};
-                ac._getChildren(['sid1', 'sid2']).then(
-                        function(controllers) {
-                            resolvedObject = controllers;
-                            isFinished = true;
-                        });
+
+                ac._getChildren(['sid1', 'sid2']).then(function (controllers) {
+                    resolvedObject = controllers;
+                    isFinished = true;
+                });
+
                 waitsFor( function (){
                     return isFinished;
                 },'service to be retrieved');
-                runs( function () {
+
+                runs(function () {
                     expect(resolvedObject).toEqual(expecting);
                     expect(ac._getChild).toHaveBeenCalledWith('sid1');
                     expect(ac._getChild).toHaveBeenCalledWith('sid2');
